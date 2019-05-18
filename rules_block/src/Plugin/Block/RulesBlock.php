22bbf7c025bed779d390faf8ff3470ccfd032be9
Logesh <logesh.ameex@gmail.com>
2019-05-18 21:12:55 +0530

    alignment

--- rules_block/src/Plugin/Block/RulesBlock.php
+++ rules_block/src/Plugin/Block/RulesBlock.php
@@ -5,6 +5,7 @@ namespace Drupal\rules_block\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
+ * Description message.
  *
  * @Block(
  *   id = "rules_block",
@@ -15,22 +16,28 @@ class RulesBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
+
   public function build() {
     $connection = \Drupal::database();
     if ($node = \Drupal::request()->attributes->get('node')) {
+      // kint($node);
       $nid = $node->id();
     }
+    $entity_obj = entity_load('node', $nid);
+    $author_name = $entity_obj->getOwner()->id();
+    kint($node);
 
     $node_titles = db_select('node_field_data', 'n')
-    ->fields('n', array('title'))
-    ->condition('n.type', 'article')
-    ->condition('n.nid', $nid, '<>')
-    ->orderBy('title', 'ASC')
-    ->orderBy('created', 'DESC')
-    ->execute()
-    ->fetchCol();
-    foreach($node_titles as $title) {
-      $markup .= "<div class='row'>".$title."</div><div>".$nid."</div><br/>";
+      ->fields('n', array('title'))
+      ->condition('n.type', 'article')
+      ->condition('n.nid', $nid, '<>')
+      ->orderBy('title', 'ASC')
+      ->orderBy('created', 'DESC')
+      ->orderBy('uid', $author_name)
+      ->execute()
+      ->fetchCol();
+    foreach ($node_titles as $title) {
+      $markup .= "<div class='row'>" . $title . "</div><div>" . $nid . "</div><br/>";
     }
     return array(
       '#type' => 'markup',
@@ -39,9 +46,10 @@ class RulesBlock extends BlockBase {
   }
 
   /**
-  * {@inheritdoc}
-  */
+   * {@inheritdoc}
+   */
   public function getCacheMaxAge() {
-        return 0;
+    return 0;
   }
+
 }
